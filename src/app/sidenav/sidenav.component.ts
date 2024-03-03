450caf794a4c4b1f731dc98fb3e7f7e2c9aac303
ToniBLopez <toniblopezdeveloper@gmail.com>
2024-03-03 10:05:09 +0100

    changes

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
@@ -7,14 +8,22 @@ import { MatListModule } from '@angular/material/list';
   selector: 'app-sidenav',
   standalone: true,
   imports: [MatSidenavModule, MatIconModule, MatListModule],
+  animations: [
+    trigger('rotate', [
+      state('open', style({ transform: 'rotate(90deg)' })),
+      state('close', style({ transform: 'rotate(0deg)' })),
+      transition('open <=> close', animate('0.3s ease'))
+    ])
+  ],
   template: `
-    <div #menuicon class="icon-container" (click)="sidenav.toggle()" (click)="closeIcon()">
+    <!-- <div #menuicon class="icon-container" (click)="sidenav.toggle()">
       <button mat-icon-button>
-        <mat-icon fontIcon="menu" />
+        <mat-icon [@rotate]="isDrawerOpened ? 'open' : 'close'">{{isDrawerOpened ? 'close' : 'menu'}}</mat-icon>
       </button>
-    </div>
+    </div> -->
     <mat-sidenav-container hasBackdrop=true>
-      <mat-sidenav #sidenav mode="side" opened>
+      <!-- <mat-sidenav #sidenav mode="side" (openedStart)="drawerOpened()" (closedStart)="drawerClosed()"> -->
+      <mat-sidenav #sidenav mode="side">
         <mat-nav-list>
           <h3 class="title">Job Analysis</h3>
           <mat-list-item>
@@ -39,12 +48,16 @@ import { MatListModule } from '@angular/material/list';
 export class SidenavComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   @ViewChild('menuicon', { static: true }) menuicon!: ElementRef;
+  // isDrawerOpened: boolean = false;
 
-  closeIcon() {
-    if(this.sidenav.opened) {
-      this.menuicon.nativeElement.style.left = '120px'
-    } else {
-      this.menuicon.nativeElement.style.left = '0'
-    }
-  }
+  // drawerOpened () {
+  //   this.isDrawerOpened = true
+  //   this.menuicon.nativeElement.style.transition = '0.29s'
+  //   this.menuicon.nativeElement.style.left = '110px'
+  // }
+  // drawerClosed () {
+  //   this.isDrawerOpened = false
+  //   this.menuicon.nativeElement.style.transition = '0.25s'
+  //   this.menuicon.nativeElement.style.left = 0
+  // }
 }
